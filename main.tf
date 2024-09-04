c775c98b588db82b1546847f16187b310b1a19b3
Ashley Conner <108750120+valardohaeris2@users.noreply.github.com>
2024-09-04 11:25:20 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,12 +1,1 @@
-terraform {
-  required_providers {
-    vault = {
-      source = "hashicorp/vault"
-      version = "4.4.0"
-    }
-  }
-}
-
-provider "vault" {
-  # Configuration options
-}
+resource "random_pet" {}
